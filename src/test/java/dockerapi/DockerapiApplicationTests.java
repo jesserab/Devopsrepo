b0886987d874d0be7875d8ab5019838087dfd18d
jesser <jesser.abidelli@etudiant-enit.utm.tn>
2022-11-26 15:20:42 +0100

    first commit

---
+++ src/test/java/dockerapi/DockerapiApplicationTests.java
@@ -0,0 +1,13 @@
+package dockerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
